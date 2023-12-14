db41a4c3b52f996a8e1449e0d2f9d85ba618da55
Suprajna Sharma <suprasubedi11@gmail.com>
2023-12-14 09:25:29 +0545

    checked mime type

--- index.js
+++ index.js
@@ -9,25 +9,15 @@ const logoUpload = upload.single("image");
 app.post("/test/upload", (req, res) => {
   logoUpload(req, res, (err) => {
     if (err) {
-      console.log(err);
-      return res.status(403).send({ error: "invalid file" });
+      // console.log(err);
+
+      return res.status(400).send({ error: "invalid file" });
     }
     //when no error
     // console.log("save the file", req.file);
-    res.send("file uploaded successfully");
+    return res.send("file uploaded successfully");
   });
 });
-// app.post("/test/upload", upload.single("image"), (err, req, res) => {
-//   if (err) {
-//     res.status(400).send(err.message);
-//   } else {
-//     res.send("uploaded successfully");
-//   }
-// });
-
-//buffer ma upload gara
-//buffer ko image lai base64 encode gara=>convert to blob
-//upload blob
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening on port:: ${port}`);
